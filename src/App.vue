a89290eafd9fba0c58cac519f67864c0d92fadb8
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-07-30 00:21:04 -0400

    add vue scaffolding

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template name="app">
+    omg its bootlebooxes!!
+    <button>button so the page doesn't spam reload</button>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'node:path'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => ['codeblock'].includes(tag)
+                }
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src/')
+        }
+    },
+    build: {
+        target: 'es2021'
+    },
+    server: {
+        port: 2000
+    }
+})
