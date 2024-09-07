24c631e682dd6005b5ebccd62d178bddfb8c86a7
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-09-06 22:56:16 -0400

    cool animations on play button

--- src/menu/MenuPlayButton.vue
+++ src/menu/MenuPlayButton.vue
@@ -1,78 +1,217 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 
-const mouseover = () => {
-    console.log('hover')
+const button = reactive<{
+    rx: number
+    ry: number
+    rz: number
+    dx: number
+    dy: number
+    dz: number
+    sc: number
+    time: number
+    state: 'bob' | 'hover' | 'click' | 'spin'
+    bobStage: number
+}>({
+    rx: 0,
+    ry: 0,
+    rz: 0,
+    dx: 0,
+    dy: 0,
+    dz: 0,
+    sc: 1,
+    time: 1000,
+    state: 'bob',
+    bobStage: -1
+});
+
+let bobTimeout: NodeJS.Timeout | undefined = undefined;
+let bobTimeout2: NodeJS.Timeout | undefined = undefined;
+const bobStates: { d: [number, number, number], r: [number, number, number] }[] = [
+    {
+        d: [0.5, 0.2, 0.1],
+        r: [-3, 3, -2]
+    },
+    {
+        d: [0.2, 0, 0],
+        r: [3, -3, 2]
+    },
+    {
+        d: [-0.5, 0.2, 0.1],
+        r: [-3, -3, -2]
+    },
+    {
+        d: [-0.2, 0, 0],
+        r: [3, 3, 2]
+    },
+];
+const updateBobAnimation = () => {
+    if (button.state != 'bob') return;
+    if (bobTimeout !== undefined) clearTimeout(bobTimeout);
+    if (bobTimeout2 !== undefined) clearTimeout(bobTimeout2);
+    bobTimeout = setTimeout(updateBobAnimation, 3000);
+    button.bobStage = (button.bobStage + 1) % bobStates.length;
+    const currState = bobStates[button.bobStage];
+    bobTimeout2 = setTimeout(() => {
+        button.dx = currState.d[0];
+        button.dy = currState.d[1];
+        button.dz = currState.d[2];
+        button.time = 3000;
+    }, 1000);
+    button.rx = currState.r[0];
+    button.ry = currState.r[1];
+    button.rz = currState.r[2];
 };
-const mouseout = () => {
-    console.log('leave')
+let popTimeout: NodeJS.Timeout | undefined = undefined;
+const updatePopAnimation = () => {
+    if (button.sc == 1) button.sc = 1.05;
+    else button.sc = 1;
+    if (popTimeout !== undefined) clearTimeout(popTimeout);
+    popTimeout = setTimeout(updatePopAnimation, button.time * 2);
+}
+updatePopAnimation();
+
+const mouseenter = () => {
+    if (button.state == 'bob') {
+        button.state = 'hover';
+        button.time = 250;
+        if (bobTimeout !== undefined) clearTimeout(bobTimeout);
+        if (bobTimeout2 !== undefined) clearTimeout(bobTimeout2);
+        button.dx = 0;
+        button.dy = 0;
+        button.dz = 1;
+        button.rx = 5;
+        button.ry = 6;
+        button.rz = -3;
+        updatePopAnimation();
+    }
 };
-const play = () => {
-    console.log('click')
+const mouseleave = () => {
+    if (button.state == 'hover') {
+        button.state = 'bob';
+        updateBobAnimation();
+        button.sc = 1.05;
+        updatePopAnimation();
+    }
 };
+const mousedown = () => {
+    button.state = 'click';
+    if (bobTimeout !== undefined) clearTimeout(bobTimeout);
+    if (bobTimeout2 !== undefined) clearTimeout(bobTimeout2);
+    if (popTimeout !== undefined) clearTimeout(popTimeout);
+    button.time = 100;
+    button.dx = 0.1;
+    button.dy = 0;
+    button.dz = -1;
+    button.rx = 15;
+    button.ry = 20;
+    button.rz = -5;
+    button.sc = 1;
+    window.addEventListener('mouseup', mouseup);
+    window.addEventListener('mouseleave', mouseup);
+};
+const mouseup = () => {
+    window.removeEventListener('mouseup', mouseup);
+    window.removeEventListener('mouseleave', mouseup);
+    if (button.state == 'click') {
+        button.state = 'spin';
+        if (bobTimeout !== undefined) clearTimeout(bobTimeout);
+        if (bobTimeout2 !== undefined) clearTimeout(bobTimeout2);
+        if (popTimeout !== undefined) clearTimeout(popTimeout);
+        button.dx = 0;
+        button.dy = 0;
+        button.dz = 0;
+        button.rx = 0;
+        button.ry = 0;
+        button.rz = 0;
+        button.sc = 1;
+        button.time = 250;
+    }
+};
+
+updateBobAnimation();
 </script>
 
 <template>
-    <div class="playButtonWrapper">
-
-        <button class="playButton" @click="play()" @mouseover="mouseover()" @mouseout="mouseout()" title="Play">
-            <!-- front face panels -->
-            <div class="p g p1 f"></div>
-            <div class="p g p2 f"></div>
-            <div class="p g p3 f"></div>
-            <div class="p g p4 f"></div>
-            <div class="p g p5 f"></div>
-            <div class="p g p6 f"></div>
-            <div class="p g p7"></div>
-            <div class="p g p8"></div>
-            <div class="p g p9 f"></div>
-            <div class="p g p10 f"></div>
-            <!-- back face panels -->
-            <div class="p g p1 b"></div>
-            <div class="p g p2 b"></div>
-            <div class="p g p3 b"></div>
-            <div class="p g p4 b"></div>
-            <div class="p g p5 b"></div>
-            <div class="p g p6 b"></div>
-            <div class="p g p11"></div>
-            <div class="p g p12"></div>
-            <div class="p g p9 b"></div>
-            <div class="p g p10 b"></div>
-            <!-- edge panels -->
-            <div class="p ddg e1 h"></div>
-            <div class="p ddg e2 h"></div>
-            <div class="p dg e3 v"></div>
-            <div class="p dg e4 v"></div>
-            <div class="p llg e5 h"></div>
-            <div class="p llg e6 h"></div>
-            <div class="p lg e7 v"></div>
-            <div class="p lg e8 v"></div>
-            <!-- center cutout panels -->
-            <div class="p lw c1"></div>
-            <div class="p dw c2"></div>
-            <div class="p w c3"></div>
-            <div class="p w c4"></div>
-        </button>
+    <div class="playButtonContainer">
+        <div class="playButtonWrapper">
+            <button class="playButton" @mousedown="mousedown()" @mouseenter="mouseenter()" @mouseleave="mouseleave()" title="Play">
+                <!-- front face panels -->
+                <div class="p g p1 f"></div>
+                <div class="p g p2 f"></div>
+                <div class="p g p3 f"></div>
+                <div class="p g p4 f"></div>
+                <div class="p g p5 f"></div>
+                <div class="p g p6 f"></div>
+                <div class="p g p7"></div>
+                <div class="p g p8"></div>
+                <div class="p g p9 f"></div>
+                <div class="p g p10 f"></div>
+                <!-- back face panels -->
+                <div class="p g p1 b"></div>
+                <div class="p g p2 b"></div>
+                <div class="p g p3 b"></div>
+                <div class="p g p4 b"></div>
+                <div class="p g p5 b"></div>
+                <div class="p g p6 b"></div>
+                <div class="p g p11"></div>
+                <div class="p g p12"></div>
+                <div class="p g p9 b"></div>
+                <div class="p g p10 b"></div>
+                <!-- edge panels -->
+                <div class="p ddg e1 h"></div>
+                <div class="p ddg e2 h"></div>
+                <div class="p dg e3 v"></div>
+                <div class="p dg e4 v"></div>
+                <div class="p llg e5 h"></div>
+                <div class="p llg e6 h"></div>
+                <div class="p lg e7 v"></div>
+                <div class="p lg e8 v"></div>
+                <!-- center cutout panels -->
+                <div class="p lw c1"></div>
+                <div class="p dw c2"></div>
+                <div class="p w c3"></div>
+                <div class="p w c4"></div>
+                <!-- big panel to make clicking button more consistent -->
+                 <div class="p a1"></div>
+            </button>
+        </div>
     </div>
 </template>
 
 <style>
-@keyframes spinnyCarrier {
+@keyframes menu-play-button-spin {
     0% {
-        transform: rotate3d(1, 1, 0, 45deg);
+        transform: none;
     }
-
+    10% {
+        transform: translate3d(0em, 1em, -4em) rotateY(-15deg) rotateX(-5deg) rotateZ(5deg);
+    }
+    25% {
+        transform: translate3d(0em, -1.5em, -4em) rotateY(360deg) rotateX(20deg);
+    }
     50% {
-        transform: rotate3d(1, 1, 0, -45deg) rotateY(360deg);
+        transform: translate3d(-1em, 0.7em, -6em) rotateY(400deg) rotateX(-30deg) rotateZ(10deg);
     }
-
+    53% {
+        transform: translate3d(-1em, 0.6em, -6em) rotateY(420deg) rotateX(0deg) rotateZ(15deg);
+    }
     100% {
-        transform: rotate3d(1, 1, 0, 45deg) rotateY(720deg);
+        transform: translate3d(1em, -15em, -4em) rotateY(1440deg) rotateX(20deg) rotateZ(-5deg);
     }
 }
 </style>
 <style scoped>
-.playButtonWrapper {
+.playButtonContainer {
     perspective: 1000px;
+    transition: v-bind("(button.time * 2) + 'ms'") ease-in-out transform;
+    transform: scale(v-bind("button.sc"));
+}
+
+.playButtonWrapper {
+    transform-style: preserve-3d;
+    transition: v-bind("button.time + 'ms'") cubic-bezier(0.4, 0, 0.6, 1) transform;
+    transform: translate3d(v-bind("button.dx + 'em'"), v-bind("button.dy + 'em'"), v-bind("button.dz + 'em'"));
 }
 
 .playButton {
@@ -87,9 +226,13 @@ const play = () => {
     transform-style: preserve-3d;
     pointer-events: none;
     background-color: transparent;
+    transition: v-bind("button.time + 'ms'") ease-in-out transform;
+    transform: rotateX(v-bind("button.rx + 'deg'")) rotateY(v-bind("button.ry + 'deg'")) rotateZ(v-bind("button.rz + 'deg'"));
+    animation: 3000ms cubic-bezier(0.2, 0, 0.6, 1) v-bind("button.state == 'spin' ? 'menu-play-button-spin' : ''");
+    animation-delay: 150ms;
+    animation-fill-mode: forwards;
+    user-select: none;
     cursor: pointer;
-
-    animation: 5000ms linear spinnyCarrier infinite;
 }
 
 .playButton>* {
@@ -279,6 +422,16 @@ const play = () => {
     width: 2em;
     height: 5em;
     transform: rotateY(90deg);
+}
+
+/* big panel to make clicking more consistent */
+.a1 {
+    top: 0em;
+    left: 0em;
+    width: 14em;
+    height: 9em;
+    transform: translateZ(1em);
+    background-color: transparent;
 }
 
 /* colors */
